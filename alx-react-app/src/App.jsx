a7cff747be6b0c5179a1037c451017c89b664b78
loice okello <loicelaura@gmail.com>
2025-02-21 12:25:44 +0300

    added footer.jsx,header.jsx,maincntent.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -6,6 +9,9 @@ import './App.css';
 function App() {
     return (
         <div className="App">
+          <Header />
+            <MainContent />
+            <Footer />
             <div>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />

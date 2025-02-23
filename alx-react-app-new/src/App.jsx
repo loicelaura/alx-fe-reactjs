ab4dbf1c632663f170003573390d2196388444a0
loice okello <loicelaura@gmail.com>
2025-02-23 15:17:19 +0300

    added counter.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,35 +1,12 @@
-import UserProfile from './components/UserProfile';
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from 'react';
+import Counter from './components/Counter'; 
 
 function App() {
   return (
-      <div className="App">
-          <Header />
-          <MainContent />
-          <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-          <Footer />
-          <div>
-              <a href="https://vitejs.dev" target="_blank">
-                  <img src={viteLogo} className="logo" alt="Vite logo" />
-              </a>
-              <a href="https://react.dev" target="_blank">
-                  <img src={reactLogo} className="logo react" alt="React logo" />
-              </a>
-          </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-              <p>
-                  Edit <code>src/App.jsx</code> and save to test HMR
-              </p>
-          </div>
-          <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      </div>
+    <div className="App">
+      <h1>Counter Application</h1>
+      <Counter /> 
+    </div>
   );
 }
 

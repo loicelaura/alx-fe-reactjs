563f3c3f1edf846bae698051c802c2a0e93ef5ca
loice okello <loicelaura@gmail.com>
2025-02-23 16:32:56 +0300

    updated userprofile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,8 @@
-const UserProfile = (props) => {
-    return (
-        <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
-        </div>
-    );
-};
+import React from 'react';
+import UserInfo from './UserInfo';
 
-export default UserProfile;
+function ProfilePage() { // Remove userData prop
+  return <UserInfo />; // Remove userData prop
+}
+
+export default ProfilePage;

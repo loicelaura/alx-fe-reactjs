84f0262806a8774a32fc9a8031c5299a873bb45c
loice okello <loicelaura@gmail.com>
2025-03-13 15:37:24 +0300

    updated userprofile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,8 @@
 // src/components/UserProfile.js
 
-// src/components/UserProfile.js
-
 function UserProfile() {
     return (
-      <div className="user-profile bg-gray-100 p-4 sm:p-8 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+      <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
         <img
           src="https://via.placeholder.com/150"
           alt="User"

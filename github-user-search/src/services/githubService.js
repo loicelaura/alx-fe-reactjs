5196351ecb33089efcd85dd37331aa2770b5485d
loice okello <loicelaura@gmail.com>
2025-03-28 09:06:34 +0300

    updated githubservice

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
-const GITHUB_API_BASE_URL = 'https://api.github.com';
-//If you needed a personal access token for higher rate limits.
-//const GITHUB_API_TOKEN = import.meta.env.VITE_APP_GITHUB_API_KEY;
+const GITHUB_API_BASE_URL = 'https://api.github.com/users';
 
-export const fetchUserRepositories = async (username) => {
+export const fetchUserData = async (username) => {
   try {
-    const response = await axios.get(`${GITHUB_API_BASE_URL}/users/${username}/repos`);
+    const response = await axios.get(`${GITHUB_API_BASE_URL}/${username}`);
     return response.data;
   } catch (error) {
-    console.error('Error fetching repositories:', error);
-    throw error; // Rethrow the error for handling in components
+    console.error('Error fetching user data:', error);
+    throw error;
   }
 };
